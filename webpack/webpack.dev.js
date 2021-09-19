3afaeeca8e174b5373a7c6176b7d54cae259b12f
Rajeev Kumar <thakurrajeev37@gmail.com>
2021-09-19 06:47:46 +0000

    Updated webpack.dev.js

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
-const BUILD_DIR = path.join(__dirname, "..","/dist");
+const BUILD_DIR = path.join(__dirname, "..","/build");
 
 
 module.exports = merge(common, {
